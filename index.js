47929ba9cb6738a6367aac9974f8d06014f6a563
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-01 23:35:11 -0700

    find multiple and single data to database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors  = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app= express();
 const port = process.env.PORT||5000;
 
@@ -29,6 +29,33 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+   const serviceCollection = client.db("carDoctor").collection("services") ;
+  
+  //  find multiple data
+   app.get('/services',async(req,res)=>{
+    const cursor = serviceCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+   })
+
+  //  find single data
+  app.get('/services/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id:new ObjectId(id)};
+    
+    const options = {
+      
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: {  title: 1, imdb: 1,
+        service_id:1,price:1, img:1},
+    };
+
+     const result = await serviceCollection.findOne(query,options);
+     res.send(result);
+     
+  })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
